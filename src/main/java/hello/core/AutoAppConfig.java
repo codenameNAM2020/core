525bb860f8466d5d4435bace4d5d7a3cd49bcb1e
codenameNAM2020 <codenam2020@gmail.com>
2022-06-02 11:10:11 +0900

    feat : 컴포넌트 스캔과 의존관계 자동 주입 시작
    - @Test

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,12 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(     //예제 코드를 살리기 위한 필터
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+}
