4a5cdbadef3192fea82eb0d1be2c2574d99e4900
codenameNAM2020 <codenam2020@gmail.com>
2022-06-02 13:32:05 +0900

    feat : 탐색 위치와 기본 스캔 대상
    - @Test

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(     //예제 코드를 살리기 위한 필터
+@ComponentScan(
+//        basePackages = "hello.core.member",
+//        basePackageClasses = AutoAppConfig.class,
+        //예제 코드를 살리기 위한 필터
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
