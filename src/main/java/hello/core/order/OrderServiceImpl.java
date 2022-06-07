64c0d360f389784e6e58f956c57ef4e0063997e1
codenameNAM2020 <codenam2020@gmail.com>
2022-06-07 18:19:12 +0900

    feat : 롬복과 최신 트랜드
    - getter, setter, 생성자 생략

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,10 +3,12 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor    // final이 붙은 파라미터를 가진 생성자를 생성함
 public class OrderServiceImpl implements OrderService {
 
     //값이 변하지 않음 (final)
@@ -16,11 +18,12 @@ public class OrderServiceImpl implements OrderService {
     // 생성자가 하나만 있다면 @Autowired가 생략된 것과 같음 (자동으로 의존관계가 주입됨)
     // 생성자는 spring life cycle에서 자동으로 등록됨
     // -> 생성자 먼저 등록되기 때문에 먼저 출력됨
+    // @RequiredArgsConstructor로 생성자 생략
 //    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
