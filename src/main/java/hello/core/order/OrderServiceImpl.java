1e052e3cbe91339efdcd98905455eaeda987eea8
codenameNAM2020 <codenam2020@gmail.com>
2022-06-07 17:52:24 +0900

    feat : 생성자 주입 선택
    - @Test

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -10,16 +10,26 @@ import org.springframework.stereotype.Component;
 public class OrderServiceImpl implements OrderService {
 
     //값이 변하지 않음 (final)
-//    private final MemberRepository memberRepository;
-//    private final DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
     // 수정자 주입(setter 주입)을 위한 final 제거
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
+//    private MemberRepository memberRepository;
+//    private DiscountPolicy discountPolicy;
 
     // 필드 주입 - DI 프레임워크가 없으면 아무것도 할 수 없음
 //    @Autowired private MemberRepository memberRepository;
 //    @Autowired private DiscountPolicy discountPolicy;
+
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
 
     // 생성자가 하나만 있다면 @Autowired가 생략된 것과 같음 (자동으로 의존관계가 주입됨)
     // 생성자는 spring life cycle에서 자동으로 등록됨
@@ -32,11 +42,11 @@ public class OrderServiceImpl implements OrderService {
 
     // 잘 사용하지 않음
     // 스프링 컨테이너가 관리하는 스프링 빈이어야 동작함(의존관계 자동 주입)
-    @Autowired
-    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired
+//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
